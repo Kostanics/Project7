717fbd9169b6d50d4beb9a327f2b3c9679756277
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-05-26 20:46:49 +0300

    Update Source.cpp

--- Project7/Source.cpp
+++ Project7/Source.cpp
@@ -6,41 +6,41 @@ using namespace std;
 
 int main() {
     vector <char> list(0, 0);
-    vector <char> list1(0, 0);
-    vector <char> list2(0, 0);
+    vector <char> L1(0, 0);
+    vector <char> L2(0, 0);
     char s1 = ' ', s2 = ' ';
-    cout << "enter for list1, to end enter '*' " << ' ' << endl;
+    cout << "enter for L1, to end enter '*' " << ' ' << endl;
     while (cin >> s1) {
-        list1.push_back(s1);
+        L1.push_back(s1);
         if (s1 == '*') {
-            list1.pop_back();
+            L1.pop_back();
             break;
         }
     }
     cout << endl;
-    cout << "list1:" << ' ';
-    for (unsigned int i = 0; i < list1.size(); ++i) {
-        cout << list1[i] << ' ';
+    cout << "L1:" << ' ';
+    for (unsigned int i = 0; i < L1.size(); ++i) {
+        cout << L1[i] << ' ';
     }
     cout << endl;
-    cout << "enter for list2, to end enter '*' " << endl;
+    cout << "enter for L2, to end enter '*' " << endl;
     while (cin >> s2) {
-        list2.push_back(s2);
+        L2.push_back(s2);
         if (s2 == '*') {
-            list2.pop_back();
+            L2.pop_back();
             break;
         }
     }
     cout << endl;
-    cout << "list2:" << ' ';
-    for (unsigned int i = 0; i < list2.size(); ++i) {
-        cout << list2[i] << ' ';
+    cout << "L2:" << ' ';
+    for (unsigned int i = 0; i < L2.size(); ++i) {
+        cout << L2[i] << ' ';
     }
     cout << endl;
-    for (unsigned int i = 0; i < list1.size(); ++i) {
-        for (unsigned int j = 0; j < list2.size(); ++j) {
-            if (list2[j] == list1[i]) {
-               list.push_back(list1[i]);
+    for (unsigned int i = 0; i < L1.size(); ++i) {
+        for (unsigned int j = 0; j < L2.size(); ++j) {
+            if (L2[j] == L1[i]) {
+               list.push_back(L1[i]);
             }
         }
     }
@@ -60,7 +60,7 @@ int main() {
         cout << "not coincidence" << ' ' << endl;
     }
     cout << endl << endl;
-    list1.clear();
-    list2.clear();
+    L1.clear();
+    L2.clear();
     list.clear();
 }
